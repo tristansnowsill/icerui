4f9372d5edab1b2a72f1a572e8a69db946826738
Tristan Snowsill <tristan.snowsill@googlemail.com>
2020-06-16 10:42:04 +0100

    Work in progress
    The following changes are work in progress:
    - Add confint.psa and iceriu functions to work with heemod::run_psa()
    results and manually specified incremental effects and costs
    - Unify documentation for similar functions
    - Expand uiplot to accept heemod::run_psa() results and manually
    specified incremental effects and costs

--- R/icerui.R
+++ R/icerui.R
@@ -1,29 +1,53 @@
-#' Calculate the ICER uncertainty interval for a BCEA object
+#' Calculate ICER uncertainty intervals
 #'
-#' @param object A BCEA object.
+#' Calculate uncertainty intervals for the incremental cost-effectiveness
+#' ratio (ICER), choosing from a number of established methods.
+#'
+#' The Fieller method is a parametric uncertainty interval which is accurate
+#' assuming incremental costs and effects follow a bivariate normal
+#' distribution. The bootstrap percentile method (\code{percentile})
+#' calculates ICERs for each simulation, orders these as suggested by Glick et
+#' al. (2015), then takes the relevant percentiles from this vector. The
+#' bootstrap acceptability method (\code{acceptability}) performs calculations
+#' similarly to the cost-effectiveness acceptability curve.
+#'
+#' @param delta.e Vector of incremental effects.
+#' @param delta.c Vector of incremental costs.
+#' @param level   The confidence level required.
+#' @param method  The method to be used to calculate the uncertainty interval.
+#' @param ...     Additional argument(s) for methods.
+#' @return        A matrix or vector with columns giving lower and upper
+#'                confidence limits for the ICER and each row giving details
+#'                for an intervention. These will be labelled as (1-level)/2
+#'                and 1 - (1-level)/2 in \% (by default 2.5\% and 97.5\%).
+#' @export
+icerui <- function(delta.e, delta.c, level = 0.95,
+                   method = c("fieller", "percentile", "acceptability"),
+                   ...) {
+  stopifnot(level > 0 & level < 1)
+  method <- match.arg(method)
+  fun <- switch(method,
+                fieller       = fieller,
+                percentile    = bspercent,
+                acceptability = bsaccept)
+  res <- fun(delta.e, delta.c, level)
+  names(res) <- sprintf("%.1f%%", 100 * c((1 - level) / 2, 1 - (1 - level) / 2))
+  res
+}
+
+#' @describeIn icerui Calculate the ICER uncertainty interval for a BCEA
+#'   object
+#'
+#' @param object For \code{confint.bcea}, the result of a \code{BCEA::bcea()}
+#'   command; for \code{confint.psa}, the result of a \code{heemod::run_psa()}
+#'   command.
 #' @param parm   Should not change from "ICER" (the default).
-#' @param level  The confidence level required.
-#' @param method The method to be used to calculate the uncertainty interval
-#'               (see Details).
 #' @param comparison  Selects the comparator, in case of more than two
 #'               interventions being analysed. Uncertainty intervals will be
-#'               produced for each versus \code{object$ref}. Default (NULL)
-#'               results in uncertainty intervals being produced for all
-#'               interventions.
-#' @param ...    Additional argument(s) for methods.
-#' @return       A matrix (or vector) with columns giving lower and upper
-#'               confidence limits for the ICER and each row giving details
-#'               for an intervention. These will be labelled as (1-level)/2
-#'               and 1 - (1-level)/2 in \% (by default 2.5\% and 97.5\%).
-#' @details      The Fieller method is a parametric uncertainty interval which
-#'               is accurate assuming incremental costs and effects follow a
-#'               bivariate normal distribution. The bootstrap percentile
-#'               method (\code{percentile}) calculates ICERs for each
-#'               simulation, orders these as suggested by Glick et al. (2015),
-#'               then takes the relevant percentiles from this vector. The
-#'               bootstrap acceptability method (\code{acceptability})
-#'               performs calculations similarly to the cost-effectiveness
-#'               acceptability curve.
+#'               produced for each versus \code{object$ref}
+#'               (\code{confint.bcea}) or versus the first defined strategy
+#'               (\code{confint.psa}). Default (NULL) results in uncertainty
+#'               intervals being produced for all interventions.
 #' @export
 confint.bcea <- function(object, parm = "ICER", level = 0.95,
                          method = c("fieller", "percentile", "acceptability"),
@@ -58,34 +82,78 @@ confint.bcea <- function(object, parm = "ICER", level = 0.95,
   res
 }
 
+#' @describeIn icerui Calculate the ICER uncertainty interval for a heemod PSA
+#'   object
+#' @export
+confint.psa <- function(object, parm = "ICER", level = 0.95,
+                        method = c("fieller", "percentile", "acceptability"),
+                        comparison = NULL, ...) {
+  stopifnot(inherits(object, "psa"))
+  # Big lazy
+  if (requireNamespace("heemod", quietly = TRUE) & requireNamespace("BCEA", quietly = TRUE)) {
+    confint.bcea(heemod::run_bcea(object), level, method, comparison, ...)
+  } else {
+    stop("heemod and/or BCEA package is not installed.")
+  }
+}
+
 #' Cost-effectiveness scatter plot with confidence intervals
 #'
-#' @param object A BCEA object.
-#' @param level  The confidence level required.
-#' @param method The method to be used to calculate the uncertainty interval
-#'               (see Details).
-#' @param comparison  Select one comparator.
-#' @param graph  Specify the graphics engine to use.
-#' @param ...    Additional argument(s) for methods.
+#' Produces a scatter plot with incremental effects on the x-axis and
+#' incremental costs on the y-axis, accompanied by the confidence intervals
+#' produced by the selected method. Can use base or ggplot2 graphics (if
+#' installed).
+#'
+#' @param object  A \code{bcea} or \code{psa} object.
+#' @param delta.e A vector of incremental effects.
+#' @param delta.c A vector of incremental costs.
+#' @param level   The confidence level required.
+#' @param method  The method to be used to calculate the uncertainty interval
+#'                (see \link{icerui}).
+#' @param comparison  Select one comparator (by index or name if \code{object}
+#'   is a \code{bcea} object, or by name if \code{object} is a \code{psa}
+#'   object).
+#' @param graph   Specify the graphics engine to use.
+#' @param ...     Additional argument(s) for methods.
 #' @export
-uiplot <- function(object, level = 0.95,
+uiplot <- function(object = NULL, delta.e = NULL, delta.c = NULL, level = 0.95,
                    method = c("fieller", "percentile", "acceptability"),
                    comparison = NULL, graph = c("base", "ggplot2"), ...) {
-  stopifnot(inherits(object, "bcea"))
-  if (is.null(comparison)) comparison <- object$comp
-  stopifnot(length(comparison) == 1)
-  method <- match.arg(method)
-  if (object$n.comparisons > 1) {
-    reindex <- which(object$comp == comparison)
-    dt <- data.frame(e = object$delta.e[, reindex], c = object$delta.c[, reindex])
-  } else {
-    dt <- data.frame(e = object$delta.e, c = object$delta.c)
+  if (!xor(is.null(delta.e) | is.null(delta.c), is.null(object))) {
+    stop("Must specify EITHER delta.e and delta.c OR object.")
   }
-  ui <- confint.bcea(object, level = level, method = method, comparison = comparison)
+
+  stopifnot(is.null(object) | inherits(object, "bcea") | inherits(object, "psa"))
+
+  if (!is.null(object) & inherits(object, "bcea")) {
+    if (is.null(comparison)) comparison <- object$comp
+    stopifnot(length(comparison) == 1)
+    if (object$n.comparisons > 1) {
+      if (is.numeric(comparison)) {
+        reindex <- which(object$comp == comparison)
+      } else {
+        reindex <- which(object$interventions[object$comp] == comparison)
+      }
+      delta.e <- object$delta.e[, reindex]
+      delta.c <- object$delta.c[, reindex]
+    } else {
+      delta.e <- object$delta.e
+      delta.c <- object$delta.c
+    }
+  }
+
+  if (!is.null(object) & inherits(object, "psa")) {
+
+  }
+
+  method <- match.arg(method)
+  ui <- icerui(delta.e, delta.c, level = level, method = method)
+
   graph <- match.arg(graph)
   if (graph == "ggplot2") {
     if (requireNamespace("ggplot2", quietly = TRUE)) {
-      p <- ggplot2::ggplot(dt, ggplot2::aes(x = e, y = c)) +
+      p <- ggplot2::ggplot(data.frame(x = delta.e, y = delta.c),
+                           ggplot2::aes(x = e, y = c)) +
         ggplot2::geom_point() +
         ggplot2::geom_abline(slope = ui[1], linetype = "dashed") +
         ggplot2::geom_abline(slope = ui[2], linetype = "dashed") +
@@ -100,10 +168,10 @@ uiplot <- function(object, level = 0.95,
     extent <- function(d) c(min(d), max(d))
     expanded_limits <- function(lim, include = 0) c(min(lim[1], include), max(lim[2], include))
     graphics::plot(
-      dt$e,
-      dt$c,
-      xlim = expanded_limits(extent(dt$e)),
-      ylim = expanded_limits(extent(dt$c)),
+      delta.e,
+      delta.c,
+      xlim = expanded_limits(extent(delta.e)),
+      ylim = expanded_limits(extent(delta.c)),
       xlab = "Incremental effectiveness",
       ylab = "Incremental costs",
       pch  = 21,
